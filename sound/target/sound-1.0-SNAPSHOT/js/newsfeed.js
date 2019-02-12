d919592ba47a324f7bcb997f9a14fbbca2a1be1e
Gabriela Rueda <gabriela.rueda@dawsoncollege.qc.ca>
2019-02-12 15:51:26 -0500

    finish news feed

--- sound/target/sound-1.0-SNAPSHOT/js/newsfeed.js
+++ sound/target/sound-1.0-SNAPSHOT/js/newsfeed.js
@@ -1,46 +1,79 @@
-function news(){
-  $(document).ready(function() {
-	//feed to parse
-	var feed = "https://rss.cbc.ca/lineup/arts.xml";
-	
-	$.ajax(feed, {
-		accepts:{
-			xml:"application/xml"
-		},
-		dataType:"rss-xml",
-                crossDomain: true,
-		success:function(data) {
-			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+function news() {
+    
+    
+    
+        //https://rss2json.com/docs
+    
+    
+        //feed to parse
+        var feed = "https://rss.cbc.ca/lineup/arts.xml";
 
-			$(data).find("item").each(function () { // or "item" or whatever suits your feed
-				var el = $(this);
-				console.log("------------------------");
-				console.log("title      : " + el.find("title").text());
-				console.log("link       : " + el.find("link").text());
-				console.log("description: " + el.find("description").text());
-			});
-	
+        $.ajax({
+            url: 'https://api.rss2json.com/v1/api.json',
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                rss_url: feed
+            }
+        }).done(function (response) {
+            if (response.status != 'ok') {
+                throw response.message;
+            }
 
-		}	
-	});
-	
-});
- 
+            console.log('====== ' + response.feed.title + ' ======');
+
+            for (var i = 0; i < 9; i++) {
+
+                var item = response.items[i];
+
+                var $new = $("<div>", {"class": "card mb-3 cards"});
+                var $header = $("<h3>", {"class": "card-header"}).append(item.title);
+                var $body = $("<div>", {"class": "card-body"});
+                //var $img = $("<img>", {"class": "imgNews"}).attr("src", item.thumbnail);
+                var $description = $("<p>", {"class": "card-text"}).append(item.description);
+                var $link = $("<a>", {"class": "card-link"}).attr("href", item.link).append("link");
+                var $footer = $("<div>", {"class": "card-footer text-muted"}).append(item.pubDate);
+                
+                //$body.append($img);
+                $body.append($description);
+                $body.append($link);
+                $new.append($header);
+                $new.append($body);
+                $new.append($footer);
+                $("#newsfeed").append($new);
+
+
+                console.log(item.title);
+                console.log(item.pubDate);
+                console.log(item.link);
+                console.log(item.description);
+                console.log(item.thumbnail);
+                console.log("===========");
+            }
+            
+            $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        accessibility: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: "#prev",
+        nextArrow: "#next"
+    });
+        });
+        
+        
+    
 }
 
 
+$(document).ready(function () {
+    
+    news();
+    
+    
+    
 
-$(document).ready(function(){
-  $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    accessibility: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    prevArrow: "#prev",
-    nextArrow: "#next"
-  });
-  
-  news();
+    
 });
