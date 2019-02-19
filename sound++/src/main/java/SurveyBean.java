5bbcbb5f9a2e78cb3053e482470ea793c07fee59
unknown <gabrielachury@gmail.com>
2019-02-19 14:50:51 -0500

    Start survey model and controller

---
+++ sound++/src/main/java/SurveyBean.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.io.Serializable;
+import javax.ejb.Stateless;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Gabriela
+ */
+@Named(value= "surveyBean")
+@SessionScoped
+public class SurveyBean implements Serializable{
+    private String question;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String option5;
+
+    @Inject
+    private DAO DAO;
+    
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    public String getOption5() {
+        return option5;
+    }
+
+    public void setOption5(String option5) {
+        this.option5 = option5;
+    }
+    
+    public String addSurvey(){
+        
+        return "survey.xhtml";
+    }
+}
