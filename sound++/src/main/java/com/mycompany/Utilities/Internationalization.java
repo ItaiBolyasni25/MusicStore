06f951d6596b3047d40e1d7d0ee10cc61f56398a
unknown <gabrielachury@gmail.com>
2019-04-07 19:26:53 -0400

    add reset string

--- sound++/src/main/java/com/mycompany/Utilities/Internationalization.java
+++ sound++/src/main/java/com/mycompany/Utilities/Internationalization.java
@@ -547,4 +547,8 @@ public class Internationalization implements Serializable {
     public String getDownload(){
         return language.equals("English")? "Download" : "Telecharger";
     }
+    
+    public String getReset(){
+        return language.equals("English")? "Reset" : "Reinitialiser";
+    }
 }
