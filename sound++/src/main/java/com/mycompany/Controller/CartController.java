c0f6ba82e5dfc6abde9280e1a70f61a0629686aa
unknown <itai.bolyasni25@gmail.com>
2019-04-08 00:51:23 -0400

    remove some system.out

--- sound++/src/main/java/com/mycompany/Controller/CartController.java
+++ sound++/src/main/java/com/mycompany/Controller/CartController.java
@@ -49,14 +49,12 @@ public class CartController implements Serializable {
     public String removeTrack(Track track, User user) {
         Cart cart = dao.find(new Cart(), "track.track_id = '" + track.getId() + "' AND identifier.user.email = '" + user.getEmail() + "'").get(0);
         dao.deleteCart(cart, true);
-        System.out.println("worked" + cart.getTrack());
         return "checkout.xhtml";
     }
 
     public String removeAlbum(Album album, User user) {
         Cart cart = dao.find(new Cart(), "album.album_id = '" + album.getId() + "' AND identifier.user.email = '" + user.getEmail() + "'").get(0);
         dao.deleteCart(cart, true);
-        System.out.println("worked" + cart.getTrack());
         return "checkout.xhtml";
     }
 
